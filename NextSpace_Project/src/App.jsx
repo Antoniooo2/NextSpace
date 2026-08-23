bb2cd2a4dd3d912d5c77ded5b0feebbb473ad52d
yami-rp <elida.rodriguez2026@hilasal.superate.org.sv>
2026-08-22 21:49:17 -0600

    app.jsx fixed

--- NextSpace_Project/src/App.jsx
+++ NextSpace_Project/src/App.jsx
@@ -5,11 +5,7 @@ import LoginForm from './components/LoginForm.jsx'
 import SignupForm from './components/SignUpForm.jsx'
 import ForgotPassword from './pages/ForgotPassword'
 import NewPassword from './pages/NewPassword'
-import Welcome from './pages/Welcome.jsx'
-<<<<<<< HEAD
 import Dashboard from './pages/Dashboard.jsx'
-=======
->>>>>>> 1cd65e055b767ce02be776f51f0670fb868aed67
 
 function MainFlow() {
   const [view, setView] = useState('landing')
@@ -46,11 +42,7 @@ function App() {
     <Routes>
       <Route path="/forgot-password" element={<ForgotPassword />} />
       <Route path="/new-password" element={<NewPassword />} />
-      <Route path="/welcome" element={<Welcome />} />
-<<<<<<< HEAD
       <Route path="/dashboard" element={<Dashboard />} />
-=======
->>>>>>> 1cd65e055b767ce02be776f51f0670fb868aed67
       <Route path="/*" element={<MainFlow />} />
     </Routes>
   )
